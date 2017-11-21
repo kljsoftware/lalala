963736f35ec8729901da8a8682f35797e7526bf2
huangzugang <huangzugang@51yjtc.com>
2017-11-21 17:12:47 +0800

    1. 初始提交

---
+++ Music/Music/Macros/GlobalMacro.swift
@@ -0,0 +1,8 @@
+//
+//  GlobalMacro.swift
+//  Music
+//
+//  Created by hzg on 2017/11/21.
+//  Copyright © 2017年 demo. All rights reserved.
+//
+
---
+++ Music/Music/View/Main/Search/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SearchViewController.swift
+//  Music
+//
+//  Created by hzg on 2017/11/21.
+//  Copyright © 2017年 demo. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
